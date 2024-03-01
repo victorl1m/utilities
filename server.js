e135067a1b9f6dba6741b1a89d940a477c6d17d1
Victor Lima <victorl1ma@hotmail.com>
2024-03-01 20:25:18 -0300

    change port

--- server.js
+++ server.js
@@ -148,6 +148,6 @@ app.get('/downloadaudio', async (req, res) => {
 });
 
 
-app.listen(3001, () => {
-  console.log('Server listening on port 3001');
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
